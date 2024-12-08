7a68aea9175a2eb2ab85074aa439d5ccb877fedd
Surizer <bm.suriya@gmail.com>
2024-12-08 17:37:54 +0100

    Iteration1, Accessing video file and saving the output with timestamp

--- src/Speeddetection.py
+++ src/Speeddetection.py
@@ -1,45 +1,49 @@
 import cv2
 import os
+import datetime
 
-#Path to the video file
-video_path=r"D:\Python_Projects\Speed_Detection_122024\data\3\video.hevc"
+# Path to the video file
+video_path = r"D:\Python_Projects\Speed_Detection_122024\data\3\video.hevc"
 
-#Output folder to save images
-output_folder =r"D:\Python_Projects\Speed_Detection_122024\Test\Iteration1"
+# Output folder to save images
+output_folder = r"D:\Python_Projects\Speed_Detection_122024\Test\Iteration1"
 os.makedirs(output_folder, exist_ok=True)
 
-#Open the video file
-cap= cv2.VideoCapture(video_path)
+# Open the video file
+cap = cv2.VideoCapture(video_path)
 if not cap.isOpened():
-    print("Error:Could not open video")
+    print("Error: Could not open video")
     exit()
 
-#Get the FPS of the vide
+# Get the FPS of the video
 fps = cap.get(cv2.CAP_PROP_FPS)
 print(f"Frames per second: {fps}")
-frame_count=0
-save_interval =int(fps * 5)
+frame_count = 0
+save_interval = int(fps * 5)
 
 while True:
-    #Read a frame from the video
+    # Read a frame from the video
     ret, frame = cap.read()
 
     if not ret:
         print("End of video or error")
         break
-    #Save every nth frame based on the time interval
-    if frame_count % save_interval ==0:
-        frame_filname = os.path.join(output_folder, f'frame_{frame_count}.jpg')
-        cv2.imwrite(frame_filname, frame)
-        print(f'saved: {frame_filname}')
+
+    # Save every nth frame based on the time interval
+    if frame_count % save_interval == 0:
+        # Generate timestamp for each frame
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        frame_filename = os.path.join(output_folder, f'Output_{timestamp}_{frame_count}.jpg')
+        cv2.imwrite(frame_filename, frame)
 
     frame_count += 1
 
-    #Display the frame
-    cv2.imshow('Video', frame )
+    # Display the frame
+    cv2.imshow('Video', frame)
 
-    #wait for the user to press q to exit
+    # Wait for the user to press 'q' to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
